4b874b5adde8fa93c23458c578b31fcec64faedc
kun.dong <329009063@qq.com>
2013-12-28 18:39:19 +0800

    Signed-off-by: kun.dong <329009063@qq.com>

--- master/kl/syntax.cpp
+++ master/kl/syntax.cpp
@@ -15,40 +15,75 @@ bool syntax_parser::init(symbols_mgr* mgr)
 
 sytax_node* syntax_parser::parser_tokens(const token_list_type& thelist) 
 {
+    init_pos(thelist);
     sytax_node* root = alloc_node();
     if (NULL == root) {
         return NULL;
     }
 
-    auto itr = thelist.begin();
-    for (; itr != thelist.end(); ++ itr)
+    sytax_node* parsed_node = NULL;
+    const token* tk = NULL;
+    while (tk = get_next_token())
     {
-        const token* tok = &*itr;
-
-        // @todo Ö´ÐÐÓï·¨½âÎö
-
-        if (tok->get_type() == token_name) 
+        if (tk->get_type() == token_name) 
         {
-            std::string name = tok->str();
-            symbol_info info;
-            bool ret;
-            ret = symbols_mgr_->get_symbol(name, info);
-            if (!ret) {
-                info.token_ = *tok;
-                symbols_mgr_->add_symbol(name, info);
-            }
+            parsed_node = parser_name_decl(tk);
         }
-        else if (tok->get_type() == token_assign) 
+        else if (tk->get_type() == token_function) 
         {
 
         }
 
+
+        root->siblings_.push_back(parsed_node);
     }
 
     return NULL;
 }
 
-sytax_node* syntax_parser::alloc_node()
+void syntax_parser::init_pos(const token_list_type& thelist)
 {
-    return new sytax_node;
+    token_list_ = & thelist;
+    itr_ = token_list_->begin();
+}
+
+sytax_node* syntax_parser::alloc_node(node_type type)
+{
+    return new sytax_node(type);
+}
+
+const token* syntax_parser::get_next_token()
+{
+    if (itr_ != token_list_->end()) {
+        const token* tk = & *itr_;
+        itr_ ++;
+        return tk;
+    }
+
+    return NULL;
+}
+
+sytax_node* syntax_parser::parser_name_decl(const token* tk)
+{
+    std::string name = tk->str();
+    symbol_info info;
+    bool ret;
+    ret = symbols_mgr_->get_symbol(name, info);
+    if (!ret) {
+        info.token_ = *tk;
+        symbols_mgr_->add_symbol(name, info);
+    }
+
+    return alloc_node();
+}
+
+sytax_node* syntax_parser::parser_function_decl(const token* tk)
+{
+    return alloc_node();
+}
+
+bool syntax_parser::is_match(const token* tk, token_type type)
+{
+
+    return false;
 }
--- master/kl/syntax.h
+++ master/kl/syntax.h
@@ -19,9 +19,19 @@ public:
     sytax_node* parser_tokens(const token_list_type& thelist) ;
 
 protected:
-    sytax_node* alloc_node();
+    void init_pos(const token_list_type& thelist);
+    sytax_node* alloc_node(node_type type = type_statement);
+    const token* get_next_token();
 
 private:
+    sytax_node* parser_name_decl(const token* tk);
+    sytax_node* parser_function_decl(const token* tk);
+    bool is_match(const token* tk, token_type type);
+
+private:
+    const token_list_type* token_list_;
+    token_list_type::const_iterator itr_;
+
     symbols_mgr* symbols_mgr_;
 };
 

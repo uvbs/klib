d0c7f414d2ee8627c72bdaeb48aab7e8ae677d33
kun.dong <329009063@qq.com>
2014-01-09 23:32:13 +0800

    Signed-off-by: kun.dong <329009063@qq.com>

--- master/core/debuglog.h
+++ master/core/debuglog.h
@@ -6,12 +6,16 @@
 #include <time.h>
 #include <locale>
 #include <windows.h>
+#include <TCHAR.H>
 
 //#define RELEASE_DEBUG
 
 namespace klib {
 namespace debug {
 
+#ifndef _countof
+#define _countof(Arr)  (sizeof(Arr)/sizeof(Arr[0]))
+#endif
 
 #if defined(_DEBUG) || defined(RELEASE_DEBUG)
 
@@ -34,6 +38,36 @@ public:
     m_line = line;
   }
 
+  bool get_app_path(char path[], int pathlen)
+  {
+      DWORD dwlen = GetModuleFileNameA(GetModuleHandle(NULL), path, pathlen);
+      int index = dwlen - 1;
+      while (index > 0 )
+      {
+          if (path[index] == '\\') {
+              path[index + 1] = '\0';
+              break;
+          }
+          -- index;
+      }
+      return true;
+  }
+
+  bool get_app_path(WCHAR path[], int pathlen)
+  {
+      DWORD dwlen = GetModuleFileNameW(GetModuleHandle(NULL), path, pathlen);
+      int index = dwlen - 1;
+      while (index > 0)
+      {
+          if (path[index] == _T('\\')) {
+              path[index + 1] = _T('\0');
+              break;
+          }
+          -- index;
+      }
+      return true;
+  }
+
   void operator ()(const char * format,...)
   {
     char newformat[512] = {0};
@@ -43,43 +77,45 @@ public:
     strcpy(newformat, format);
     va_list arg_ptr;
     va_start(arg_ptr, format);
-    _vsnprintf_s(msgbuffer, sizeof(msgbuffer), newformat, arg_ptr);
+    _vsnprintf(msgbuffer, sizeof(msgbuffer), newformat, arg_ptr);
     va_end(arg_ptr);
 
     //写入到文件中
-    tm newtime;
-    __time64_t long_time;
+    time_t long_time;
     char timebuf[26];
 
-    _time64( &long_time );
-    _localtime64_s( &newtime, &long_time );
+    time( &long_time );
+    tm* newtime = localtime(&long_time );
 
     //得到日期的字符串
-    char logfile[30] = {0};
-    sprintf_s(logfile,
-              sizeof(logfile),
-              "%d-%d-%d_log.txt",
-              newtime.tm_year + 1900,
-              newtime.tm_mon + 1,
-              newtime.tm_mday);
+    char file_name[1024];
+    char logfile[1024] = {0};
+
+    get_app_path(file_name, _countof(file_name));
+    sprintf(logfile,
+              "%s%d-%d-%d_log.txt",
+              file_name,
+              newtime->tm_year + 1900,
+              newtime->tm_mon + 1,
+              newtime->tm_mday);
 
     FILE* fp = NULL;
-    fopen_s(&fp, logfile, "a");
+    fp = fopen(logfile, "a");
     if(fp == NULL) {
 	  return;
 	}
 
     char logMsg[4*1024 + 30] = {0};
-    sprintf_s(timebuf, sizeof(timebuf),
+    sprintf(timebuf, 
               "%d-%d-%d %d:%d:%d ",
-              newtime.tm_year + 1900,
-              newtime.tm_mon + 1,
-              newtime.tm_mday,
-              newtime.tm_hour,
-              newtime.tm_min,
-              newtime.tm_sec);
-    sprintf_s(logMsg, sizeof(logMsg), "%s: %s \r\n", timebuf, msgbuffer);
-    fprintf_s(fp, "%s", logMsg);
+              newtime->tm_year + 1900,
+              newtime->tm_mon + 1,
+              newtime->tm_mday,
+              newtime->tm_hour,
+              newtime->tm_min,
+              newtime->tm_sec);
+    sprintf(logMsg, "%s: %s \r\n", timebuf, msgbuffer);
+    fprintf(fp, "%s", logMsg);
     fclose(fp);
 
     DWORD dwWritten = 0; 
@@ -102,42 +138,44 @@ public:
       _tcscpy(newformat, format);
       va_list arg_ptr;
       va_start(arg_ptr, format);
-      _vstprintf_s(msgbuffer, _countof(msgbuffer), newformat, arg_ptr);
+      _vstprintf(msgbuffer, newformat, arg_ptr);
       va_end(arg_ptr);
 
       //写入到文件中
-      tm newtime;
-      __time64_t long_time;
+      time_t long_time;
       TCHAR timebuf[26];
 
-      _time64( &long_time );
-      _localtime64_s( &newtime, &long_time );
+      time( &long_time );
+      tm* newtime = localtime(&long_time );
 
       //得到日期的字符串
-      TCHAR logfile[30] = {0};
-      _stprintf_s(logfile,
-          _countof(logfile),
-          _T("%d-%d-%d_log.txt"),
-          newtime.tm_year + 1900,
-          newtime.tm_mon + 1,
-          newtime.tm_mday);
+      TCHAR file_name[1024];
+      TCHAR logfile[1024] = {0};
+
+      get_app_path(file_name, _countof(file_name));
+      _stprintf(logfile,
+          _T("%s%d-%d-%d_log.txt"),
+          file_name,
+          newtime->tm_year + 1900,
+          newtime->tm_mon + 1,
+          newtime->tm_mday);
 
       FILE* fp = NULL;
-      _tfopen_s(&fp, logfile, _T("a"));
+      fp = _tfopen(logfile, _T("a"));
       if(fp == NULL) {
           return;
       }
 
       TCHAR logMsg[4*1024 + 30] = {0};
-      _stprintf_s(timebuf, _countof(timebuf),
+      _stprintf(timebuf, 
           _T("%d-%d-%d %d:%d:%d "),
-          newtime.tm_year + 1900,
-          newtime.tm_mon + 1,
-          newtime.tm_mday,
-          newtime.tm_hour,
-          newtime.tm_min,
-          newtime.tm_sec);
-      _stprintf_s(logMsg, _countof(logMsg), _T("%s: %s \r\n"), timebuf, msgbuffer);
+          newtime->tm_year + 1900,
+          newtime->tm_mon + 1,
+          newtime->tm_mday,
+          newtime->tm_hour,
+          newtime->tm_min,
+          newtime->tm_sec);
+      _stprintf(logMsg,  _T("%s: %s \r\n"), timebuf, msgbuffer);
       _ftprintf(fp, _T("%s"), logMsg);
       fclose(fp);
 

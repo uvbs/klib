b9de0c1d97c053fec0060e5c5c1fe96f9e900191
kun.dong <329009063@qq.com>
2014-01-14 22:42:00 +0800

    Signed-off-by: kun.dong <329009063@qq.com>

---
+++ master/core/ui/window.h
@@ -0,0 +1,26 @@
+#ifndef _klib_window_h_
+#define _klib_window_h_
+
+
+namespace klib {
+namespace ui {
+
+
+class window
+{
+public:
+
+    static bool has_fullscrenn_wnd();
+
+};
+
+
+
+
+
+}}
+
+
+
+
+#endif // _klib_window_h_

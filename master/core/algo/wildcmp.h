bbe7527bdfd30a31cfd0398808e0333a884600fc
kun.dong <329009063@qq.com>
2014-01-15 09:49:49 +0800

    Signed-off-by: kun.dong <329009063@qq.com>

---
+++ master/core/algo/wildcmp.h
@@ -0,0 +1,37 @@
+
+
+
+int wildcmp(const char *wild, const char *string) 
+{
+  // Written by Jack Handy - <A href="mailto:jakkhandy@hotmail.com">jakkhandy@hotmail.com</A>
+  const char *cp = NULL, *mp = NULL;
+
+  while ((*string) && (*wild != '*')) {
+    if ((*wild != *string) && (*wild != '?')) {
+      return 0;
+    }
+    wild++;
+    string++;
+  }
+
+  while (*string) {
+    if (*wild == '*') {
+      if (!*++wild) {
+        return 1;
+      }
+      mp = wild;
+      cp = string+1;
+    } else if ((*wild == *string) || (*wild == '?')) {
+      wild++;
+      string++;
+    } else {
+      wild = mp;
+      string = cp++;
+    }
+  }
+
+  while (*wild == '*') {
+    wild++;
+  }
+  return !*wild;
+}

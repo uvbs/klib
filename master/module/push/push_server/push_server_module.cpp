282070cd51576c83c03a0cbfa1949fa9d5b05b48
kun.dong <329009063@qq.com>
2014-03-11 22:13:12 +0800

    Signed-off-by: kun.dong <329009063@qq.com>

---
+++ master/module/push/push_server/push_server_module.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "push_server_module.h"
+
+
+push_server_module::push_server_module(void)
+{
+}
+
+
+push_server_module::~push_server_module(void)
+{
+}
---
+++ master/module/push/push_server/push_server_module.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class push_server_module : public module_imp
+{
+public:
+    push_server_module(void);
+    ~push_server_module(void);
+
+public:
+    BEGIN_INTERFACE_ENTRY
+        //INTERFACE_ENTRY()
+    END_INTERFACE_ENTRY
+
+protected:
+
+};
+

1e5907a9c55c97899de8e0e83f0fceb0c9f50f3f
kun.dong <329009063@qq.com>
2014-08-23 13:43:29 +0800

    modify uuid

--- master/core/core/uuid.h
+++ master/core/core/uuid.h
@@ -4,43 +4,35 @@
 
 #include "../inttype.h"
 
-#ifdef WIN32
-#else
-#include <uuid/uuid.h>
-#endif
 
 namespace klib{
 namespace core{
 
-inline string create_uuid()
-{
-    char buf[64] = {0};
-#ifdef WIN32
-    GUID guid;
-    if (S_OK == CoCreateGuid(&guid))
-    {
-        _snprintf(buf, sizeof(buf),
-            "%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-            guid.Data1, guid.Data2, guid.Data3,
-            guid.Data4[0], guid.Data4[1],
-            guid.Data4[2], guid.Data4[3],
-            guid.Data4[4], guid.Data4[5],
-            guid.Data4[6], guid.Data4[7]);
-    }
-#else
-    uuid_t uu;  
-    uuid_generate(uu);
-    if (!uuid_is_null(uu)) 
-    {
-        snprintf(buf, sizeof(buf),
-            "%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-            uu[0], uu[1], uu[2], uu[3], uu[4], uu[5], uu[6], uu[7], 
-            uu[8], uu[9], uu[10], uu[11], uu[12], uu[13], uu[14], uu[15]);
-    }
-#endif
+// 创建uuid
+std::string create_uuid();
 
-    return buf;
-}
+
+// twitter snowflake算法
+// 64       63--------------22---------12---------0
+// 符号位   |     41位时间   |10位机器码|12位自增码|
+extern uint64_t get_time();
+
+class unique_id_t
+{
+public:
+    unique_id_t();
+    ~unique_id_t();
+
+    void set_epoch(uint64_t epoch);
+    void set_machine(int32_t machine);
+    int64_t generate();
+
+private:
+    uint64_t epoch_;
+    uint64_t time_;
+    int32_t machine_;
+    int32_t sequence_;
+};
 
 }}
 

036e4b3d86a8d81899be25ee63cffd76129bd3f3
kun.dong <329009063@qq.com>
2014-09-13 21:33:03 +0800

    add function

--- master/core/util/rand_str.cpp
+++ master/core/util/rand_str.cpp
@@ -17,9 +17,20 @@ std::random_device g_rd;
 rand_str::rand_str() 
     : str_tbl_(nullptr)
     , tbl_len_(0)
+    , gen_len_(10)
 {
     str_tbl_ = g_rand_str_tbl;
     tbl_len_ = strlen(g_rand_str_tbl);
+}
+
+rand_str::rand_str(size_t len) 
+    : str_tbl_(nullptr)
+    , tbl_len_(0)
+{
+    str_tbl_ = g_rand_str_tbl;
+    tbl_len_ = strlen(g_rand_str_tbl);
+
+    gen_len_ = len;
 }
 
 bool rand_str::set_str_tbl(const char* buf, size_t len)
@@ -40,4 +51,9 @@ std::string rand_str::get_rand_str(size_t len)
         str_tmp.append(str_tbl_ + offset, 1);
     }
     return std::move(str_tmp);
+}
+
+rand_str::operator std::string()
+{
+    return get_rand_str(gen_len_);
 }

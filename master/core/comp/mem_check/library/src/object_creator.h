3847dc61097d7754e65d411dd87e040771940859
kun.dong <329009063@qq.com>
2014-09-23 15:16:27 +0800

    change

--- master/core/comp/mem_check/library/src/object_creator.h
+++ master/core/comp/mem_check/library/src/object_creator.h
@@ -12,13 +12,20 @@ public:
     typedef object_creator<T>       self_type;
     typedef unsigned long long *    pointer_type;
 
-    object_creator() : m_desc(nullptr) {}
-    object_creator(const char* desc)
-        : m_desc(desc)
+    object_creator() 
+        : m_file_name(nullptr) 
+        , m_type_name(nullptr)
+        , m_file_line(0)
+    {}
+
+    object_creator(const char* file_name, const char* tp_name, size_t line)
+        : m_file_name(file_name)
+        , m_type_name(tp_name)
+        , m_file_line(line)
     {}
 
 #define  check_get_ptr(Type)  \
-    T* ptr = (T*)mem_lib::instance()->alloc_global(sizeof(T), m_desc); \
+    T* ptr = (T*)mem_lib::instance()->alloc_global(sizeof(T), to_desc()); \
     if (nullptr == ptr)         \
     {                           \
         return ptr;             \
@@ -88,12 +95,45 @@ public:
     }
 
 protected:
-    const char*  m_desc;
+    const char* to_desc()
+    {
+        if (nullptr == m_file_name)
+        {
+            return nullptr;
+        }
+
+        char* cur_pos = (char*) strrchr(m_file_name, '\\');
+        if (nullptr == cur_pos)
+        {
+            cur_pos = strncpy(desc_buff, m_file_name, _countof(desc_buff));
+        }
+        else 
+        {
+            ++ cur_pos;
+            cur_pos = strncpy(desc_buff, cur_pos, _countof(desc_buff));
+        }
+
+        cur_pos = strcat(cur_pos, ":");
+        cur_pos = strcat(cur_pos, m_type_name);
+        cur_pos = strcat(cur_pos, ":");
+        
+        char tmp[20];
+        _itoa(m_file_line, tmp, 10);
+        cur_pos = strcat(cur_pos, tmp);
+
+        return desc_buff;
+    }
+
+protected:
+    const char*  m_file_name;
+    const char*  m_type_name;
+    size_t       m_file_line;
+    char         desc_buff[1024];
 };
 
 
 //@todo 需要记录行数，类型名，及大小
-#define Type(t)  object_creator<t>()
+#define Type(t)  object_creator<t>(##__FILE__, #t, ##__LINE__)
 
 
 
